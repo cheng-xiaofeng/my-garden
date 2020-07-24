e85b31b6202f3e2d464e27904b2b64cf63fa2eb6
xiaofeng.cheng <xiaofeng.cheng@keymantech.cn>
2020-07-24 13:45:00 +0800

    Updates

---
+++ precache-manifest.a7b2e1e614fbae0ffa04230116cbf8b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3b9cbdb8d2876dee0f794226392f92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81de1bcb440c903cbc3b",
+    "url": "./static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "55c369c4b8bfa25b8786",
+    "url": "./static/css/main.7e4a964a.chunk.css"
+  },
+  {
+    "revision": "81de1bcb440c903cbc3b",
+    "url": "./static/js/2.80cd2c4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.80cd2c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c369c4b8bfa25b8786",
+    "url": "./static/js/main.c2a28053.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
